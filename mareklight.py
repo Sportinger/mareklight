de01efd0bc19b957b75bb41275d73e9d3cb2fdea
Sportinger <roman.kuskowski@gmail.com>
2024-01-04 19:51:25 +0100

    2

--- mareklight.py
+++ mareklight.py
@@ -2,7 +2,7 @@ import os
 import tkinter as tk
 
 def ping_ip(ip_address):
-    response = os.system("ping -n 1 " + ip_address)
+    response = os.system("ping -n 1 -w 500 " + ip_address)  # Send 1 echo request with a timeout of 500 ms
     if response == 0:
         return True
     else:
@@ -16,11 +16,12 @@ def update_window_color(window, ip_address):
 
 def create_window(ip_address):
     window = tk.Tk()
+    window.title('mareklight')  # Set window title
     window.geometry('200x200')
     update_window_color(window, ip_address)
-    window.after(1000, lambda: update_window_color(window, ip_address))  # Update color every second
+    window.after(60000, lambda: update_window_color(window, ip_address))  # Update color every minute
     window.mainloop()
 
 if __name__ == "__main__":
-    ip_address = '8.8.8.8'  # Replace with your IP address
+    ip_address = '192.168.145.20'  # Replace with your IP address
     create_window(ip_address)
